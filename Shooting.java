57523feeceba70de7aea9a9a3149f53fed84d267
Harsh Dabas <iamharshdabas@gmail.com>
2023-01-06 19:04:01 +0530

    Progress

--- Shooting.java
+++ Shooting.java
@@ -3,50 +3,143 @@ class Shooting
 {
 	public static void main(String[] args)
 	{
+		int choiceOuter, choiceInner;
+		char sighter, status;
 		Scanner input = new Scanner(System.in);
-		System.out.print("Choose training type\nMatch\n[1] 10M\n[2] 25M\n[3] 50M\n[4] Custom\nEnter 1, 2, 3 or 4: ");
-		int choice = input.nextInt();
-		switch (choice)
+		System.out.print("Choose training type\n[1] 10M\n[2] 25M\n[3] 50M\n[4] Custom\nEnter 1, 2, 3 or 4: ");
+		choiceOuter = input.nextInt();
+		switch (choiceOuter)
 		{
 			case 1:
-				match10();
+				System.out.print("Do you want sighter time ?\n[y/n]: ");
+				sighter = input.next().charAt(0);
+				System.out.print("Are you issf ?\n[y/n]: ");
+				status = input.next().charAt(0);
+				air(sighter, status);
 				break;
 			case 2:
-				match25();
+				System.out.print("[1] center\n[2] sports\n[3] standard\n[4] rapid\nEnter 1, 2, 3 or 4: ");
+				choiceInner = input.nextInt();
+				System.out.print("Do you want sighter time ?\n[y/n]: ");
+				sighter = input.next().charAt(0);
+				System.out.print("Are you issf ?\n[y/n]: ");
+				status = input.next().charAt(0);
+				switch (choiceInner)
+				{
+					case 1:
+						center(sighter, status);
+						break;
+					case 2:
+						sports(sighter, status);
+						break;
+					case 3:
+						standard(sighter, status);
+						break;
+					case 4:
+						rapid(sighter, status);
+						break;
+				}
 				break;
 			case 3:
-				match50();
+				System.out.print("Do you want sighter time ?\n[y/n]: ");
+				sighter = input.next().charAt(0);
+				System.out.print("Are you issf ?\n[y/n]: ");
+				status = input.next().charAt(0);
+				free(sighter, status);
 				break;
 			case 4:
 				custom();
 				break;
-			default:
-				System.out.println("INVALID INPUT");
 		}
 	}
-	static void match10()
+	static void air(char sighter, char status)
 	{
-	}
-	static void match25()
-	{
-		Scanner input = new Scanner(System.in);
-		System.out.print("Choose training type\nMatch\n[1] 10M\n[2] 25M\n[3] 50M\nCustom\n[4] 10M\n[5] 25M\n[6] 50M\nEnter 1, 2, 3, 4, 5 or 6: ");
-		int choice = input.nextInt();
-		switch (choice)
+		if (sighter == "Y" || sighter == "y")
 		{
-			case 1:
-				break;
-			case 2:
-				break;
-			case 3:
-				break;
-			case 4:
-				break;
-			default:
-				System.out.println("INVALID INPUT");
+			// TODO
+		}
+		if (status == "Y" || status == "y")
+		{
+			// TODO
+		}
+		else
+		{
+			// TODO
 		}
 	}
-	static void match50()
+	static void center(char sighter, char status)
 	{
+		if (sighter == "Y" || sighter == "y")
+		{
+			// TODO
+		}
+		if (status == "Y" || status == "y")
+		{
+			// TODO
+		}
+		else
+		{
+			// TODO
+		}
+	}
+	static void sports(char sighter, char status)
+	{
+		if (sighter == "Y" || sighter == "y")
+		{
+			// TODO
+		}
+		if (status == "Y" || status == "y")
+		{
+			// TODO
+		}
+		else
+		{
+			// TODO
+		}
+	}
+	static void standard(char sighter, char status)
+	{
+		if (sighter == "Y" || sighter == "y")
+		{
+			// TODO
+		}
+		if (status == "Y" || status == "y")
+		{
+			// TODO
+		}
+		else
+		{
+			// TODO
+		}
+	}
+	static void rapid(char sighter, char status)
+	{
+		if (sighter == "Y" || sighter == "y")
+		{
+			// TODO
+		}
+		if (status == "Y" || status == "y")
+		{
+			// TODO
+		}
+		else
+		{
+			// TODO
+		}
+	}
+	static void free(char sighter, char status)
+	{
+		if (sighter == "Y" || sighter == "y")
+		{
+			// TODO
+		}
+		if (status == "Y" || status == "y")
+		{
+			// TODO
+		}
+		else
+		{
+			// TODO
+		}
 	}
 }
