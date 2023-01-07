b3e36f5519bb36eccc103bc002b38eeda26e3cff
Harsh Dabas <iamharshdabas@gmail.com>
2023-01-07 11:21:23 +0530

    Removed stuff

--- Shooting.java
+++ Shooting.java
@@ -1,3 +1,4 @@
+import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
 class Shooting
 {
@@ -8,138 +9,9 @@ class Shooting
 		Scanner input = new Scanner(System.in);
 		System.out.print("Choose training type\n[1] 10M\n[2] 25M\n[3] 50M\n[4] Custom\nEnter 1, 2, 3 or 4: ");
 		choiceOuter = input.nextInt();
-		switch (choiceOuter)
-		{
-			case 1:
-				System.out.print("Do you want sighter time ?\n[y/n]: ");
-				sighter = input.next().charAt(0);
-				System.out.print("Are you issf ?\n[y/n]: ");
-				status = input.next().charAt(0);
-				air(sighter, status);
-				break;
-			case 2:
-				System.out.print("[1] center\n[2] sports\n[3] standard\n[4] rapid\nEnter 1, 2, 3 or 4: ");
-				choiceInner = input.nextInt();
-				System.out.print("Do you want sighter time ?\n[y/n]: ");
-				sighter = input.next().charAt(0);
-				System.out.print("Are you issf ?\n[y/n]: ");
-				status = input.next().charAt(0);
-				switch (choiceInner)
-				{
-					case 1:
-						center(sighter, status);
-						break;
-					case 2:
-						sports(sighter, status);
-						break;
-					case 3:
-						standard(sighter, status);
-						break;
-					case 4:
-						rapid(sighter, status);
-						break;
-				}
-				break;
-			case 3:
-				System.out.print("Do you want sighter time ?\n[y/n]: ");
-				sighter = input.next().charAt(0);
-				System.out.print("Are you issf ?\n[y/n]: ");
-				status = input.next().charAt(0);
-				free(sighter, status);
-				break;
-			case 4:
-				custom();
-				break;
-		}
 	}
-	static void air(char sighter, char status)
+	static sleep(int seconds)
 	{
-		if (sighter == "Y" || sighter == "y")
-		{
-			// TODO
-		}
-		if (status == "Y" || status == "y")
-		{
-			// TODO
-		}
-		else
-		{
-			// TODO
-		}
-	}
-	static void center(char sighter, char status)
-	{
-		if (sighter == "Y" || sighter == "y")
-		{
-			// TODO
-		}
-		if (status == "Y" || status == "y")
-		{
-			// TODO
-		}
-		else
-		{
-			// TODO
-		}
-	}
-	static void sports(char sighter, char status)
-	{
-		if (sighter == "Y" || sighter == "y")
-		{
-			// TODO
-		}
-		if (status == "Y" || status == "y")
-		{
-			// TODO
-		}
-		else
-		{
-			// TODO
-		}
-	}
-	static void standard(char sighter, char status)
-	{
-		if (sighter == "Y" || sighter == "y")
-		{
-			// TODO
-		}
-		if (status == "Y" || status == "y")
-		{
-			// TODO
-		}
-		else
-		{
-			// TODO
-		}
-	}
-	static void rapid(char sighter, char status)
-	{
-		if (sighter == "Y" || sighter == "y")
-		{
-			// TODO
-		}
-		if (status == "Y" || status == "y")
-		{
-			// TODO
-		}
-		else
-		{
-			// TODO
-		}
-	}
-	static void free(char sighter, char status)
-	{
-		if (sighter == "Y" || sighter == "y")
-		{
-			// TODO
-		}
-		if (status == "Y" || status == "y")
-		{
-			// TODO
-		}
-		else
-		{
-			// TODO
-		}
+		TimeUnit.SECONDS.sleep(seconds);
 	}
 }
