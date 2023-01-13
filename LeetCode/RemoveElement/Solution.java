58b9d4a3bce981ecbd6454fc9e8dcd118bda1ac3
Harsh Dabas <iamharshdabas@gmail.com>
2023-01-13 21:05:00 +0530

    Dont know what to say

---
+++ LeetCode/RemoveElement/Solution.java
@@ -0,0 +1,16 @@
+class Solution
+{
+	public int removeElement(int[] number, int target)
+	{
+		int pointer = 0;
+		for (int exitLoop = 0; exitLoop < number.length; exitLoop++)
+		{
+			if (number[exitLoop] != target)
+			{
+				number[pointer] = number[exitLoop];
+				pointer++;
+			}
+		}
+		return pointer;
+	}
+}
