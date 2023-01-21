23a8c143571635861813e3f3d050ddb8928d34b9
Harsh Dabas <iamharshdabas@gmail.com>
2023-01-21 16:04:41 +0530

    Done

---
+++ LeetCode/FindFirstAndLastPositionOfElementInSortedArray/Solution.java
@@ -0,0 +1,25 @@
+class Solution
+{
+	public int[] searchRange(int[] array, int target)
+	{
+		boolean firstDone = false;
+		int first = -1, last = -1;
+		for (int exitLoop = 0; exitLoop < array.length; exitLoop++)
+		{
+			if (array[exitLoop] == target)
+			{
+				if (!firstDone)
+				{
+					first = exitLoop;
+					firstDone = true;
+				}
+				last = exitLoop;
+			}
+			else if (firstDone)
+			{
+				break;
+			}
+		}
+		return new int[]{first, last};
+	}
+}
