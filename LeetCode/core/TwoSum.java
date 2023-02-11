48beff75a4778de6d4cbbfad19cf6b963e1ebdb1
Harsh Dabas <108268580+IamHARSHDABAS@users.noreply.github.com>
2023-02-11 18:26:02 +0530

    Update TwoSum.java

--- LeetCode/core/TwoSum.java
+++ LeetCode/core/TwoSum.java
@@ -5,8 +5,8 @@ package core;
 public class TwoSum{
 
     public int[] find(int[] array, int target){
-        for (int i: array) {
-            for (int j: array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
                 if (array[i] + array[j] == target) return new int[] {i, j};
             }
         }
