51d4a5e95d8efa3f909f788e3271f2b80a7cdccf
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-02-11 17:22:50 +0530

    Enhanced for loop

--- Matrix/core/RankOne.java
+++ Matrix/core/RankOne.java
@@ -3,9 +3,9 @@ package core;
 public class RankOne {
 
     public int rank(int[][] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                if (array[i][j] != 0) return array[i][j];                
+        for (int[] i : array) {
+            for (int j : i) {
+                if (j != 0) return j;
             }
         }
         return 0;
